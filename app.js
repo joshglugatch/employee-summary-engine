e98d0617870a1e4f6dff299de10af373f3ce4be4
Josh Glugatch <joshglugatch@gmail.com>
2020-10-12 19:00:38 -0700

    add inquirer prompts and necessary functions

--- app.js
+++ app.js
@@ -9,14 +9,170 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { resolve } = require("path");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+function promptManager(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Manager Name:"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Manager ID:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Manager Email:"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Manager Office Number:"
+        },
+        {
+            type: "list",
+            name: "another",
+            message: "Would you like to add an engineer or intern?",
+            choices: ["Engineer","Intern","None"]
+        }
+    ])
+}
+
+function promptEngineer(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Engineer Name:"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Engineer ID:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Engineer Email:"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Engineer GitHub Name:"
+        },
+        {
+            type: "list",
+            name: "another",
+            message: "Would you like to add another engineer or intern?",
+            choices: ["Engineer","Intern","None"]
+        }
+    ])
+}
+
+function promptIntern(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Intern Name:"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Intern ID:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Intern Email:"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Intern School:"
+        },
+        {
+            type: "list",
+            name: "another",
+            message: "Would you like to add another engineer or intern?",
+            choices: ["Engineer","Intern","None"]
+        }
+    ])
+}
+
+const team = [];
+
+function runEngineer(){
+    promptEngineer().then(function(response){
+       
+        if(response.another == "Engineer"){
+            team.push(JSON.stringify(response));
+            runEngineer();
+        }else if(response.another == "Intern"){
+            team.push(JSON.stringify(response));
+            runIntern();
+        }else if(response.another == "None"){
+            team.push(JSON.stringify(response));
+            console.log("Generating team...");
+            console.log(team)
+            return;
+        }
+    })
+}
+
+function runIntern(){
+    promptIntern().then(function(response){
+       
+        if(response.another == "Engineer"){
+            team.push(JSON.stringify(response));
+            runEngineer();
+        }else if(response.another == "Intern"){
+            team.push(JSON.stringify(response));
+            runIntern();
+        }else if(response.another == "None"){
+            team.push(JSON.stringify(response));
+            console.log("Generating team...");
+            console.log(team)
+            return;
+        }
+    })
+}
+
+function runManager(){
+    promptManager().then(function(response){
+        
+        if(response.another == "Engineer"){
+            team.push(JSON.stringify(response));
+            runEngineer();
+        }else if(response.another == "Intern"){
+            team.push(JSON.stringify(response));
+            runIntern();
+        }else if(response.another == "None"){
+            team.push(JSON.stringify(response));
+            console.log("Generating team...");
+            console.log(team)
+            return;
+        }
+    })
+}
+
+
+
+runManager();
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
